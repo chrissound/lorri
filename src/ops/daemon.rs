296b17a12c8034740a2b4e37ed466313eda6c798
Philip Patsch <philip.patsch@tweag.io>
2019-11-05 16:57:45 +0100

    ops/ExitError: distinguish more error codes & apply to codebase
    We want to return specific error codes for different kinds of errors
    we are expecting. We use the codes spec’ed out by execline for its
    executables, plus 101 and 126
    
    1: expected error
    100: user error
    101: rust panic (programming error)
    111: temporary error
    126: environment not set up correctly
    127: executable missing

--- src/ops/daemon.rs
+++ src/ops/daemon.rs
@@ -16,7 +16,7 @@ pub fn main() -> OpResult {
     let socket_path = ::socket::path::SocketPath::from(&daemon_socket_file);
     // TODO: move listener into Daemon struct?
     let listener = listener::Listener::new(&socket_path).map_err(|e| match e {
-        ::socket::path::BindError::OtherProcessListening => ExitError::errmsg(format!(
+        ::socket::path::BindError::OtherProcessListening => ExitError::user_error(format!(
             "Another daemon is already listening on the socket at {}. \
              We are currently only allowing one daemon to be running at the same time.",
             socket_path.display()
